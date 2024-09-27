a831814c02aeba921e3ff1a089664882426fde89
Oswaldo <oswaldo.hernandez1410@gmail.com>
2024-09-27 15:21:04 -0600

    version con google maps

---
+++ static_map.js
@@ -0,0 +1,11 @@
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 21.814237222301667, lng: -102.76977000494668 },
+    zoom: 8,
+  });
+}
+initMap();
